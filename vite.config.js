743686dcf5c68d36a18cb1ac0d4532f894989181
morr0350 <tony.morrow@gmail.com>
2026-09-13 23:05:26 -0500

    More Vercel headaches

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default {
     },
     build:
     {
-        outDir: 'src/dist', // Output in the dist/ folder
+        outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
